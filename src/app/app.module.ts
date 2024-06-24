7cbebd28b49a93f399b664b91a3928a617dc5f42
dau240702 <huyhu240702@gmail.com>
2024-06-24 23:21:08 +0700

    feat:add page about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HeaderComponent } from './shared/header/header.component';
     AppComponent,
     FooterComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
