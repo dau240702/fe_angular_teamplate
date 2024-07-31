461b747646e6914d1469b5ffcf4d8b33b10b810f
dau240702 <huyhu240702@gmail.com>
2024-07-31 08:31:35 +0700

    add ui page admin

--- src/app/admin/pages/category-management/category-management.component.ts
+++ src/app/admin/pages/category-management/category-management.component.ts
@@ -14,17 +14,25 @@ import { Category } from '../../../../model/category.model';
 })
 export class CategoryManagementComponent implements OnInit {
   userId: number | null = null;
-  addCategoryForm: FormGroup | undefined;
-  editCategoryForm: FormGroup ;
+  addCategoryForm: FormGroup;
+  editCategoryForm: FormGroup;
   currentCategoryId: number | null = null;
-    categories = [
-    { categoryId: 1, name: 'Category 1' },
+  categories: Category[] = [
+    { categoryId: 1, name: 'Category 1', description: '', parentId: undefined },
   ];
+  filteredCategories: Category[] = [];
   parentCategories: Category[] = [];
   @ViewChild('editTemplate', { static: true }) editTemplate: any;
   @ViewChild('deleteTemplate', { static: true }) deleteTemplate: any;
-
-  columns: any[] = [];
+  columns = [
+    { prop: 'categoryId', name: 'ID' },
+    { prop: 'name', name: 'Tên thể loại' },
+    { prop: 'description', name: 'Mô tả' },
+    { prop: 'parentId', name: 'Thể loại cha' },
+    { name: 'Edit' },
+    { name: 'Delete' }
+  ];
+  offset = 0;
 
   constructor(
     private apiService: ApiService,
@@ -35,28 +43,52 @@ export class CategoryManagementComponent implements OnInit {
     this.addCategoryForm = this.fb.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
-      categoryId: [null, Validators.required] 
+      categoryId: [undefined, Validators.required]
     });
     this.editCategoryForm = this.fb.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
-      categoryId: [null, Validators.required]  
+      categoryId: [undefined, Validators.required]
     });
   }
+
+  ngOnInit(): void {
+    this.userService.initializeUserFromToken();
+    this.userService.userId$.subscribe((userId) => {
+      if (!this.userId && userId) {
+        this.userId = userId;
+        this.filteredCategories = this.categories;
+        this.loadCategories();
+        this.loadParentCategories();
+      }
+    });
+  }
+
+  updateFilter(event?: any) {
+    const val = event ? event.target.value.toLowerCase() : '';
+    this.filteredCategories = this.categories.filter(category => {
+      return category.name.toLowerCase().includes(val) || category.description.toLowerCase().includes(val);
+    });
+  }
+
+  onPage(event: any) {
+    this.offset = event.offset;
+  }
+
   addCategory() {
     if (this.addCategoryForm && this.addCategoryForm.valid) {
       const formValue = this.addCategoryForm.value;
       const categoryData: Partial<Category> = {
         name: formValue.name,
         description: formValue.description,
-        parentId: formValue.categoryId // Sử dụng trực tiếp giá trị categoryId
+        parentId: formValue.categoryId
       };
-  
+
       this.apiService.post(ConstService.AddCategory, categoryData).subscribe(
         (response: Category) => {
           this.notificationService.success('Thêm thể loại thành công.');
           this.loadCategories();
-          this.addCategoryForm?.reset();
+          this.addCategoryForm.reset();
           const modalCloseButton = document.querySelector('#exampleModaladd .btn-close') as HTMLElement;
           modalCloseButton?.click();
         },
@@ -66,13 +98,15 @@ export class CategoryManagementComponent implements OnInit {
       );
     }
   }
+
   isParentCategory(categoryName: string): boolean {
     return categoryName === 'sản phẩm' || categoryName === 'tin tức';
   }
+
   loadParentCategories() {
     this.apiService.get(ConstService.GetAllCategory).subscribe(
       (response: Category[]) => {
-        this.parentCategories = response.filter(category => 
+        this.parentCategories = response.filter(category =>
           category.name === 'sản phẩm' || category.name === 'tin tức'
         );
       },
@@ -82,39 +116,19 @@ export class CategoryManagementComponent implements OnInit {
     );
   }
 
-  getCategoryName(parentId: number | null): string {
-    if (parentId === null) {
-        return '';
+  getCategoryName(parentId: number | undefined): string {
+    if (parentId === undefined) {
+      return '';
     }
     const parentCategory = this.categories.find(cat => cat.categoryId === parentId);
-    return parentCategory ? parentCategory.name : 'Không tìm ts hấy';
-}
-
-  ngOnInit(): void {
-    this.columns = [
-      { prop: 'categoryId', name: 'ID' },
-      { prop: 'name', name: 'Name' }, 
-      { prop: 'description', name: 'Description' },
-      { prop: 'parent', name: 'Thể loại cha' },
-      { name: 'Edit', cellTemplate: this.editTemplate },
-      { name: 'Delete', cellTemplate: this.deleteTemplate }
-    ];
-
-    this.userService.initializeUserFromToken();
-    this.userService.userId$.subscribe((userId) => {
-      if (!this.userId && userId) {
-        this.userId = userId;
-        this.loadCategories();
-        this.loadParentCategories() 
-        
-      }
-    });
+    return parentCategory ? parentCategory.name : '';
   }
 
   loadCategories() {
     this.apiService.get(`${ConstService.GetAllCategory}`).subscribe(
-      (data) => {
+      (data: Category[]) => {
         this.categories = data;
+        this.updateFilter();
       },
       (error) => {
         console.error('Error fetching categories:', error);
@@ -145,10 +159,10 @@ export class CategoryManagementComponent implements OnInit {
       this.apiService.put(`${ConstService.UpdateCategory}/${this.currentCategoryId}`, categoryData).subscribe(
         (response: Category) => {
           this.notificationService.success('Chỉnh sửa thể loại thành công.');
-          this.loadCategories();  // Tải lại danh sách thể loại
+          this.loadCategories();
           this.editCategoryForm.reset();
           const modalCloseButton = document.querySelector('#exampleModaledit .btn-close') as HTMLElement;
-          modalCloseButton?.click();  // Đóng modal
+          modalCloseButton?.click();
         },
         (error) => {
           this.notificationService.error('Có lỗi xảy ra khi chỉnh sửa thể loại.');
@@ -165,40 +179,22 @@ export class CategoryManagementComponent implements OnInit {
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
-      confirmButtonText: 'Có, xóa nó đi!',
-      cancelButtonText: 'Không, giữ nguyên'
+      confirmButtonText: 'Vâng, xóa nó!'
     }).then((result) => {
       if (result.isConfirmed) {
         this.apiService.delete(`${ConstService.DeleteCategory}/${categoryId}`).subscribe(
-          () => {
-            this.categories = this.categories.filter(category => category.categoryId !== categoryId);
-            Swal.fire(
-              'Đã xóa!',
-              'Dữ liệu của bạn đã được xóa.',
-              'success'
-            );
+          (response) => {
+            this.notificationService.success('Xóa thể loại thành công.');
+            this.loadCategories();
           },
           (error) => {
-            console.error('Error deleting category:', error);
-            let errorMessage = 'Có lỗi xảy ra khi xóa dữ liệu.';
-
-            if (error.status === 400 && error.error.message.includes('constraint')) {
-              errorMessage = 'Bạn không thể xóa hàng này vì nó đang liên kết với dữ liệu khác.';
-            }
-
-            Swal.fire(
-              'Lỗi!',
-              errorMessage,
-              'error'
-            );
+            this.notificationService.error('Có lỗi xảy ra khi xóa thể loại.');
           }
         );
       }
     });
   }
-
   rowClassFunction = (row: any, index: number) => {
     return index % 2 === 0 ? 'datatable-row-even' : 'datatable-row-odd';
   }
 }
-
--- src/app/admin/pages/main-layout/main-layout.module.ts
+++ src/app/admin/pages/main-layout/main-layout.module.ts
@@ -5,6 +5,10 @@ import { RouterModule } from '@angular/router';
 import { AppAdminSharedModule } from '../../shared/app-admin.share.module';
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
 import { MainLayoutComponent } from './main-layout.component';
+import { CategoryManagementModule } from '../category-management/category-management.module';
+import { ProductManagementComponent } from '../product-management/product-management.component';
+import { ProductManagementModule } from '../product-management/product-management.module';
+import { ContactManagementModule } from '../contact-management/contact-management.module';
 
 @NgModule({
   declarations: [MainLayoutComponent],
@@ -14,6 +18,9 @@ import { MainLayoutComponent } from './main-layout.component';
     RouterModule,
     MainLayoutRoutingModule,
     AppAdminSharedModule,
+    CategoryManagementModule,
+    ProductManagementModule,
+    ContactManagementModule
   ],
   exports: [MainLayoutComponent],
 })
